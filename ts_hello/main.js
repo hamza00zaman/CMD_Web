eed7b7298f1d94a4b70726249d99a1d353c60b97
hamza00zaman <168165146+hamza00zaman@users.noreply.github.com>
2024-08-01 07:07:20 -0400

    Add files via upload

---
+++ ts_hello/main.js
@@ -0,0 +1,6 @@
+function log(message) {
+    console.log(message);
+}
+
+var message = "hamza";
+log(message);
---
+++ ts_hello/main.ts
@@ -0,0 +1,8 @@
+enum Color {
+    Red = "red",
+    Blue = "blue",
+    Green = "green",
+    Yellow = "yellow"
+};
+let backgroundColor = Color.Red;
+console.log(backgroundColor); // Output: "red"
